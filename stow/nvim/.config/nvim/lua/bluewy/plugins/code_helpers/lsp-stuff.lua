d99c2101217a6ba236ff5a8fc3117ce75d053108
BluewyDiamond <bluewy.s.network@gmail.com>
2024-05-20 13:18:25 +0200

    feat(nvim): ensure lua language server is installed

--- stow/nvim/.config/nvim/lua/bluewy/plugins/code_helpers/lsp-stuff.lua
+++ stow/nvim/.config/nvim/lua/bluewy/plugins/code_helpers/lsp-stuff.lua
@@ -29,9 +29,18 @@ return {
    {
       "williamboman/mason-lspconfig.nvim",
       lazy = false,
+
       opts = {
          auto_install = true,
       },
+
+      config = function()
+         require("mason-lspconfig").setup {
+            ensure_installed = {
+               "lua_ls"
+            }
+         }
+      end
    },
 
    {
