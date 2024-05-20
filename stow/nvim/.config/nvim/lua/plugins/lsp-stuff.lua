3e41ed54f4e922e16240eb0b475f2beafcb449d4
BluewyDiamond <bluewy.s.network@gmail.com>
2024-05-20 13:00:58 +0200

    fix(nvim): copy and paste correct plugin

--- stow/nvim/.config/nvim/lua/plugins/lsp-stuff.lua
+++ stow/nvim/.config/nvim/lua/plugins/lsp-stuff.lua
@@ -3,7 +3,7 @@ return {
       "williamboman/mason.nvim",
 
       dependencies = {
-         "williamboman/mason-lspconfig.nvim",
+         "WhoIsSethDaniel/mason-tool-installer.nvim",
       },
 
       lazy = false,
