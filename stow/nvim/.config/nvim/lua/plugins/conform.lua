dfdb43941511c77861ab9eeb8a82deb109e3ce59
BluewyDiamond <bluewy.s.network@gmail.com>
2024-05-20 12:46:11 +0200

    feat(nvim): replace none-ls with conform + nvim-lint

---
+++ stow/nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+   "stevearc/conform.nvim",
+   opts = {},
+
+   config = function()
+      require("conform").setup({
+         formatters_by_ft = {
+            lua = { "stylua" },
+         },
+      })
+
+      vim.keymap.set("n", "<leader>fm", function()
+         require("conform").format({ lsp_fallback = true })
+      end, { desc = "format files" })
+   end,
+}
