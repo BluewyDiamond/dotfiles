67ac808bb859011184f8f2c972bb5566a701bfbf
BluewyDiamond <bluewy.s.network@gmail.com>
2024-06-10 02:12:25 +0200

    style(nvim)

--- stow/nvim/.config/nvim/lua/bluewy/plugins/utils/telescope.lua
+++ stow/nvim/.config/nvim/lua/bluewy/plugins/utils/telescope.lua
@@ -5,23 +5,24 @@ return {
 
       config = function()
          vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+         vim.keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
+         vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { noremap = true, silent = true })
+         vim.keymap.set("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { noremap = true, silent = true })
+         vim.keymap.set("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { noremap = true, silent = true })
+
          vim.keymap.set(
             "n",
             "<leader>fa",
             "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
             { noremap = true, silent = true }
          )
-         vim.keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
-         vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { noremap = true, silent = true })
+
          vim.keymap.set(
             "n",
             "<leader>fz",
             "<cmd>Telescope current_buffer_fuzzy_find<CR>",
             { noremap = true, silent = true }
          )
-
-         vim.keymap.set("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { noremap = true, silent = true })
-         vim.keymap.set("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { noremap = true, silent = true })
       end,
    },
 
