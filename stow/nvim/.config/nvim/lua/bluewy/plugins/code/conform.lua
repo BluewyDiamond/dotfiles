2d1bc8258b5ee113ab685f2398b1f55101b468c2
BluewyDiamond <bluewy.s.network@gmail.com>
2024-06-10 02:03:36 +0200

    feat(nvim): specify formatters to use

--- stow/nvim/.config/nvim/lua/bluewy/plugins/code/conform.lua
+++ stow/nvim/.config/nvim/lua/bluewy/plugins/code/conform.lua
@@ -5,10 +5,17 @@ return {
    config = function()
       require("conform").setup({
          formatters_by_ft = {
+            -- common
             lua = { "stylua" },
             fish = { "fish_indent" },
 
+            -- low level
             rust = { "rustfmt" },
+
+            -- high level
+            javascript = { "prettier" },
+            typescript = { "prettier" },
+            python = { "black" },
          },
       })
 
