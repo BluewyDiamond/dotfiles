cc9861c469c459338ec092c57ba7fe0770ecea19
BluewyDiamond <bluewy.s.network@gmail.com>
2024-06-25 22:35:45 +0200

    feat(nvim): make things more fluid

--- stow/nvim/.config/nvim/lua/bluewy/plugins/code/lsp/mason-lspconfig.lua
+++ stow/nvim/.config/nvim/lua/bluewy/plugins/code/lsp/mason-lspconfig.lua
@@ -2,10 +2,6 @@ return {
    "williamboman/mason-lspconfig.nvim",
    lazy = false,
 
-   opts = {
-      auto_install = true,
-   },
-
    config = function()
       require("mason-lspconfig").setup({
          ensure_installed = {
@@ -13,13 +9,17 @@ return {
          },
       })
 
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       require("mason-lspconfig").setup_handlers({
          -- The first entry (without a key) will be the default handler
          -- and will be called for each installed server that doesn't have
          -- a dedicated handler.
 
          function(server_name) -- default handler (optional)
-            require("lspconfig")[server_name].setup({})
+            require("lspconfig")[server_name].setup({
+               capabilities = capabilities,
+            })
          end,
 
          -- Next, you can provide a dedicated handler for specific servers.
@@ -27,6 +27,31 @@ return {
          -- ["rust_analyzer"] = function()
          --    require("rust-tools").setup({})
          -- end,
+
+         ["lua_ls"] = function()
+            require("lspconfig")["lua_ls"].setup({
+               capabilities = capabilities,
+
+               settings = {
+                  Lua = {
+                     diagnostics = {
+                        globals = { "vim" },
+                     },
+
+                     workspace = {
+                        library = {
+                           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                           [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                        },
+                     },
+                  },
+               },
+            })
+         end,
+
+         ["rust_analyzer"] = function()
+            -- We leave it blank so it does not conflict with rustace.
+         end,
       })
    end,
 }
--- stow/nvim/.config/nvim/lua/bluewy/plugins/code/lsp/nvim-lspconfig.lua
+++ stow/nvim/.config/nvim/lua/bluewy/plugins/code/lsp/nvim-lspconfig.lua
@@ -1,28 +1,4 @@
 return {
    "neovim/nvim-lspconfig",
    lazy = false,
-
-   config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      lspconfig.lua_ls.setup({
-         capabilities = capabilities,
-
-         settings = {
-            Lua = {
-               diagnostics = {
-                  globals = { "vim" },
-               },
-
-               workspace = {
-                  library = {
-                     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                     [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                  },
-               },
-            },
-         },
-      })
-   end,
 }
