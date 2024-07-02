178a204cacb1916a58aec184aaa496e9fb13f0e8
BluewyDiamond <bluewy.s.network@gmail.com>
2024-07-02 03:30:26 +0200

    refactor(post)

--- post/change_locale.fish
+++ post/change_locale.fish
@@ -1,21 +1,27 @@
 set LOCALE_CONF_FILE "/etc/locale.conf"
 set LOCALE_GEN_FILE "/etc/locale.gen"
+set DEFAULT_LOCALE_FILE /etc/default/locale
 
-if not test -e $HOME/.old
-   mkdir $HOME/.old
+if test -e $LOCALE_GEN_FILE
+    sudo mv $LOCALE_GEN_FILE "$LOCALE_GEN_FILE.old"
+    touch $LOCALE_GEN_FILE
+    echo "en_IE.UTF-8 UTF-8" | suddo tee -a $LOCALE_GEN_FILE
+    sudo locale-gen
+else
+    echo "script: unable to find $LOCALE_GEN_FILE -> skipping..."
 end
 
-cp $LOCALE_GEN_FILE $HOME/.old
-cp $LOCALE_CONF_FILE $HOME/.old
 
-sudo sed -i -e 's/^\([^#].*\)/#\1/' $LOCALE_GEN_FILE
-sudo sed -i -e "/^#en_IE.UTF-8.*/s/^#//" $LOCALE_GEN_FILE
+if test -e $LOCALE_CONF_FILE
+    sudo mv $LOCALE_CONF_FILE "$LOCALE_CONF_FILE.old"
+    sudo touch $LOCALE_CONF_FILE
+    echo "LANG=en_IE.UTF-8" | sudo tee -a $LOCALE_CONF_FILE
+else
+    echo "script: unable to find $LOCALE_CONF_FILE -> skipping..."
+end
 
-sudo locale-gen
-
-sudo rm $LOCALE_CONF_FILE
-sudo touch $LOCALE_CONF_FILE
-echo "LANG=en_IE.UTF-8" | sudo tee -a $LOCALE_CONF_FILE
-
-# This is for the case scenario where a default is set.
-echo "" | sudo tee /etc/default/locale
+if not test -e $DEFAULT_LOCALE_FILE
+    echo "" | sudo tee $DEFAULT_LOCALE_FILE
+else
+    echo "script: unable to find $DEFAULT_LOCALE_FILE -> skipping..."
+end
