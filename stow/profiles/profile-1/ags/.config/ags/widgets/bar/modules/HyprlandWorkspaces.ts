958d765345a65d609bc263396c6945ef7f1c49cb
BluewyDiamond <bluewy.s.network@gmail.com>
2024-07-06 21:08:49 +0200

    feat(ags): remove extra workspace

--- stow/profiles/profile-1/ags/.config/ags/widgets/bar/modules/HyprlandWorkspaces.ts
+++ stow/profiles/profile-1/ags/.config/ags/widgets/bar/modules/HyprlandWorkspaces.ts
@@ -11,36 +11,33 @@ export default () =>
       child: Widget.Box({
          className: "workspaces",
 
-         children: ["", "󱎓", "󰋎", "", "", "", "", "󰨞"].map(
-            (value, index) => {
-               // because i start counting from the edges of the number row,
-               // so the final accessible workspace would be 10 and not 8.
-               if (index > 4) {
-                  index = index + 2;
-               }
-
-               let workspace_id = index + 1;
-
-               return Widget.Button({
-                  attribute: workspace_id,
-                  label: `${value}`,
-                  onClicked: () => dispatch(workspace_id),
-
-                  setup: (self) =>
-                     self.hook(hyprland, () => {
-                        self.toggleClassName(
-                           "active",
-                           hyprland.active.workspace.id === workspace_id
-                        );
-
-                        self.toggleClassName(
-                           "occupied",
-                           (hyprland.getWorkspace(workspace_id)?.windows || 0) >
-                              0
-                        );
-                     }),
-               });
+         children: ["", "󱎓", "󰋎", "", "", "", "󰨞"].map((value, index) => {
+            // because i start counting from the edges of the number row,
+            // so the final accessible workspace would be 10 and not 8.
+            if (index > 3) {
+               index = index + 3;
             }
-         ),
+
+            let workspace_id = index + 1;
+
+            return Widget.Button({
+               attribute: workspace_id,
+               label: `${value}`,
+               onClicked: () => dispatch(workspace_id),
+
+               setup: (self) =>
+                  self.hook(hyprland, () => {
+                     self.toggleClassName(
+                        "active",
+                        hyprland.active.workspace.id === workspace_id
+                     );
+
+                     self.toggleClassName(
+                        "occupied",
+                        (hyprland.getWorkspace(workspace_id)?.windows || 0) > 0
+                     );
+                  }),
+            });
+         }),
       }),
    });
