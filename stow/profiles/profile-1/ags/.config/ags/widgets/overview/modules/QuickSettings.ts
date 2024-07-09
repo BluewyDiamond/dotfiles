c6da34eb65901503f2ba322c8ae9f596314786f4
BluewyDiamond <bluewy.s.network@gmail.com>
2024-07-09 18:06:28 +0200

    feat(ags): implement pages for quick settings

--- stow/profiles/profile-1/ags/.config/ags/widgets/overview/modules/QuickSettings.ts
+++ stow/profiles/profile-1/ags/.config/ags/widgets/overview/modules/QuickSettings.ts
@@ -12,9 +12,10 @@ function button(text: string) {
    });
 }
 
-export default () => {
+const currentPage = Variable(0)
+
+function page1() {
    return Widget.Box({
-      className: "overview-quick-settings-container",
       hexpand: true,
       vertical: true,
       spacing: 8,
@@ -30,4 +31,66 @@ export default () => {
          }),
       ],
    });
+}
+
+const createDotButton = (index: number) => {
+   return Widget.Button({
+      className: currentPage.bind().as((v) => (v == index ? "dotbutton active" : "dotbutton")),
+      hexpand: false,
+      label: "●",
+      onClicked: () => currentPage.setValue(index),
+   });
+}
+
+export default () => {
+   let pages = {
+      page1: page1(),
+      page2: page1()
+   }
+
+   const numberOfPages = Object.keys(pages).length;
+   const pageNames = Array.from({ length: numberOfPages }, (_, i) => `page${i + 1}`);
+
+   const stack = Widget.Stack({
+      className: "overview-quick-settings-area",
+      hexpand: true,
+      // @ts-expect-error
+      shown: currentPage.bind().as((v) => `page${v + 1}`),
+      transition: "slide_left_right",
+      transitionDuration: 200,
+      children: pages,
+   });
+
+   const dotButtons = pageNames.map((_, index) => createDotButton(index));
+
+   const eventBox = Widget.EventBox({
+      onScrollUp: () => currentPage.setValue(Math.min(currentPage.value + 1, numberOfPages - 1)),
+      onScrollDown: () => currentPage.setValue(Math.max(currentPage.value - 1, 0)),
+
+      child: Widget.Box({
+         orientation: Gtk.Orientation.VERTICAL,
+         children: [
+            stack,
+            Widget.Box({
+               className: "overview-quick-settings-dots-container",
+               hpack: "center",
+               children: dotButtons,
+            })
+         ],
+
+         setup: (self) => {
+            for (let page in pageNames) {
+               // @ts-expect-error
+               self.keybind(`${Number(page.replace("page", "")) + 1}`, () => {
+                  currentPage.setValue(Number(page.replace("page", "")));
+               });
+            }
+         }
+      })
+   });
+
+   return Widget.Box({
+      className: "overview-quick-settings-container",
+      child: eventBox
+   })
 };
