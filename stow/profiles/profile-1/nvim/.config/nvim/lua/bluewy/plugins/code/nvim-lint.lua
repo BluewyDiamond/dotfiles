9f2497870892ff980804c04de5c1c60b3b0dcd6c
BluewyDiamond <bluewy.s.network@gmail.com>
2024-07-10 17:16:36 +0200

    feat(nvim): add linter for typescript

--- stow/profiles/profile-1/nvim/.config/nvim/lua/bluewy/plugins/code/nvim-lint.lua
+++ stow/profiles/profile-1/nvim/.config/nvim/lua/bluewy/plugins/code/nvim-lint.lua
@@ -7,6 +7,8 @@ return {
 
       lint.linters_by_ft = {
          fish = { "fish" },
+
+         typescript = { "eslint" },
       }
 
       local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
