07cd30ca5e6506dd56bd06409eafbb7df90e96f7
BluewyDiamond <bluewy.s.network@gmail.com>
2024-07-12 20:20:34 +0200

    chore(post)

--- post/packages/get_common_packages.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env fish
-
-set -g JSON_FILE (dirname (status -f))'/./packages.json'
-
-jq -r '[.common | .. | .std? // empty | .[]] + [.common | .. | .aur? // empty | .[]] | unique | .[]' $JSON_FILE | sort -u
