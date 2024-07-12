40dbd30fba1eadcff3177f2dff147e5923aa1ff2
BluewyDiamond <bluewy.s.network@gmail.com>
2024-07-12 20:21:31 +0200

    feat: update

--- run_all_scripts_and_setups.fish
+++ run_all_scripts_and_setups.fish
@@ -3,7 +3,7 @@
 ./post/change_keyboard.fish
 ./post/change_locale.fish
 ./post/change_vsconsole_font.fish
-./post/install_packages.fish
+./post/install_packages_v2.fish
 
 for module in $PWD/stow/profiles/profile-1/*
     stow $module -t $HOME
