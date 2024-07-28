0b1b985df00da022b6c40fdc65f2795f7edbab65
BluewyDiamond <bluewy.s.network@gmail.com>
2024-07-28 21:01:33 +0200

    feat(ags-ayulr): change more defaults

--- stow/modules/ags-ayulr/.config/ags-ayulr/options.ts
+++ stow/modules/ags-ayulr/.config/ags-ayulr/options.ts
@@ -65,7 +65,7 @@ const options = mkOptions(OPTIONS, {
     bar: {
         flatButtons: opt(true),
         position: opt<"top" | "bottom">("top"),
-        corners: opt(50),
+        corners: opt(100),
         transparent: opt(false),
         layout: {
             start: opt<Array<import("widget/bar/Bar").BarWidget>>([
