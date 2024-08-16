af8bac04c58b4743a739b38cf4a02f9275e53d3a
BluewyDiamond <bluewy.s.network@gmail.com>
2024-08-16 18:06:16 +0200

    feat(copy): update

--- copy/copy.fish
+++ copy/copy.fish
@@ -3,3 +3,8 @@
 sudo cp (dirname (status -f))/./usr/share/X11/xkb/symbols/bluewy /usr/share/X11/xkb/symbols
 sudo cp (dirname (status -f))/./usr/share/kbd/keymaps/colemak_dh_wide_custom_iso.map.gz /usr/share/kbd/keymaps
 sudo cp (dirname (status -f))/./usr/share/kbd/keymaps/colemak_dh_wide_custom_ansi.map.gz /usr/share/kbd/keymaps
+
+mkdir $HOME/.config/fish
+mkdir $HOME/.config/conf.d
+mkdir $HOME/.config/functions
+mkdir $HOME/.config/themes
