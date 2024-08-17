0ac2796c52316764806c0ea6daf93ec572cce5cf
BluewyDiamond <bluewy.s.network@gmail.com>
2024-08-17 17:34:10 +0200

    feat(ags-ayulr): disable shadows as default

--- stow/modules/ags-ayulr/.config/ags-ayulr/options.ts
+++ stow/modules/ags-ayulr/.config/ags-ayulr/options.ts
@@ -49,7 +49,7 @@ const options = mkOptions(OPTIONS, {
             opacity: opt(0),
         },
 
-        shadows: opt(true),
+        shadows: opt(false),
         padding: opt(7),
         spacing: opt(12),
         radius: opt(11),
