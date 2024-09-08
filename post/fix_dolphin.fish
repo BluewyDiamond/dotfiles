9998f20afcc842fe478a5b338bd732c9797f23a0
BluewyDiamond <bluewy.s.network@gmail.com>
2024-09-08 23:32:21 +0200

    feat: update

--- post/fix_dolphin.fish
+++ post/fix_dolphin.fish
@@ -1,6 +1,6 @@
 #!/bin/env fish
 
 # source: https://www.reddit.com/r/kde/comments/1bd313p/comment/l1jj937/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-sudo pacman -S archlinux-xdg-menu
+sudo pacman -S --nedeed archlinux-xdg-menu
 
 XDG_MENU_PREFIX=arch- kbuildsycoca6
---
+++ post/use_wezterm_dolphin.fish
@@ -0,0 +1,7 @@
+#!/bin/env fish
+
+mkdir -p ~/.local/bin
+curl -L -o ~/.local/bin/open-wezterm-here https://raw.githubusercontent.com/wez/wezterm/main/assets/open-wezterm-here
+chmod +x ~/.local/bin/open-wezterm-here
+
+echo "todo: unfinished script"
