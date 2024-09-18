030772570cfcdd88636017af1ddaf7bbced148f9
BluewyDiamond <bluewy.s.network@gmail.com>
2024-09-18 21:15:38 +0200

    feat: update

--- link_files.fish
+++ link_files.fish
@@ -12,28 +12,38 @@ if string match -q -i -- $choice y
     mkdir $HOME/.config/fish/themes
 end
 
-sudo pacman -S --needed stow
 
-pushd ./link/stow
+echo "script: stow?"
+read -P "script => Input -- " choice
 
-for module in *
-    stow $module -t $HOME
-end
+if string match -q -i -- $choice y
+    sudo pacman -S --needed stow
 
-popd
+    pushd ./link/stow
 
-# manual stuff
-
-mkdir $HOME/.librewolf
-
-ln -sf $PWD/link/manual/librewolf/librewolf.overrides.cfg $HOME/.librewolf/librewolf.overrides.cfg
-
-set target_dirs (find $HOME/.librewolf -type d -name '*default-default' 2>/dev/null)
-
-for dir in $target_dirs
-    if not test -d "$dir/chrome"
-        mdkir "$dir/chrome"
+    for module in *
+        stow $module -t $HOME
     end
 
-    ln -sf $PWD/link/manual/librewolf/userChrome.css "$dir/chrome"
+    popd
+end
+
+
+echo "script: proceed with manual stuff to link?"
+read -P "script => Input -- " choice
+
+if string match -q -i -- $choice y
+    mkdir -p $HOME/.cachy
+
+    ln -sf $PWD/link/manual/cachy-browser/cachy.overrides.cfg $HOME/.cachy/cachy.overrides.cfg
+
+    set target_dirs (find $HOME/.cachy -type d -name '*default-release' 2>/dev/null)
+
+    for dir in $target_dirs
+        if not test -d "$dir/chrome"
+            mkdir -p "$dir/chrome"
+        end
+
+        ln -sf $PWD/link/manual/cachy-browser/userChrome.css "$dir/chrome"
+    end
 end
