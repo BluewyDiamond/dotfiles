e486c66905f56e0f4b4891b0db124797b3f2d82e
BluewyDiamond <bluewy.s.network@gmail.com>
2024-09-20 00:44:40 +0200

    feat!(ags-ayulr): remove more stuff

--- link/stow/ags-ayulr/.config/ags-ayulr/widget/bar/buttons/SystemIndicators.ts
+++ link/stow/ags-ayulr/.config/ags-ayulr/widget/bar/buttons/SystemIndicators.ts
@@ -1,73 +1,69 @@
-import PanelButton from "../PanelButton"
-import icons from "lib/icons"
-import asusctl from "service/asusctl"
+import PanelButton from "../PanelButton";
+import icons from "lib/icons";
 
-const notifications = await Service.import("notifications")
-const audio = await Service.import("audio")
-const powerprof = await Service.import("powerprofiles")
+const notifications = await Service.import("notifications");
+const audio = await Service.import("audio");
+const powerprof = await Service.import("powerprofiles");
 
 const ProfileIndicator = () => {
-    const visible = asusctl.available
-        ? asusctl.bind("profile").as(p => p !== "Balanced")
-        : powerprof.bind("active_profile").as(p => p !== "balanced")
+   const visible = powerprof.bind("active_profile").as((p) => p !== "balanced");
+   const icon = powerprof
+      .bind("active_profile")
+      .as((p) => icons.powerprofile[p]);
 
-    const icon = asusctl.available
-        ? asusctl.bind("profile").as(p => icons.asusctl.profile[p])
-        : powerprof.bind("active_profile").as(p => icons.powerprofile[p])
+   return Widget.Icon({ visible, icon });
+};
 
-    return Widget.Icon({ visible, icon })
-}
+const MicrophoneIndicator = () =>
+   Widget.Icon()
+      .hook(
+         audio,
+         (self) =>
+            (self.visible =
+               audio.recorders.length > 0 || audio.microphone.is_muted || false)
+      )
+      .hook(audio.microphone, (self) => {
+         const vol = audio.microphone.is_muted ? 0 : audio.microphone.volume;
+         const { muted, low, medium, high } = icons.audio.mic;
+         const cons = [
+            [67, high],
+            [34, medium],
+            [1, low],
+            [0, muted],
+         ] as const;
+         self.icon = cons.find(([n]) => n <= vol * 100)?.[1] || "";
+      });
 
-const ModeIndicator = () => {
-    if (!asusctl.available) {
-        return Widget.Icon({
-            setup(self) {
-                Utils.idle(() => self.visible = false)
-            },
-        })
-    }
+const DNDIndicator = () =>
+   Widget.Icon({
+      visible: notifications.bind("dnd"),
+      icon: icons.notifications.silent,
+   });
 
-    return Widget.Icon({
-        visible: asusctl.bind("mode").as(m => m !== "Hybrid"),
-        icon: asusctl.bind("mode").as(m => icons.asusctl.mode[m]),
-    })
-}
+const AudioIndicator = () =>
+   Widget.Icon().hook(audio.speaker, (self) => {
+      const vol = audio.speaker.is_muted ? 0 : audio.speaker.volume;
+      const { muted, low, medium, high, overamplified } = icons.audio.volume;
+      const cons = [
+         [101, overamplified],
+         [67, high],
+         [34, medium],
+         [1, low],
+         [0, muted],
+      ] as const;
+      self.icon = cons.find(([n]) => n <= vol * 100)?.[1] || "";
+   });
 
-const MicrophoneIndicator = () => Widget.Icon()
-    .hook(audio, self => self.visible =
-        audio.recorders.length > 0
-        || audio.microphone.is_muted
-        || false)
-    .hook(audio.microphone, self => {
-        const vol = audio.microphone.is_muted ? 0 : audio.microphone.volume
-        const { muted, low, medium, high } = icons.audio.mic
-        const cons = [[67, high], [34, medium], [1, low], [0, muted]] as const
-        self.icon = cons.find(([n]) => n <= vol * 100)?.[1] || ""
-    })
-
-const DNDIndicator = () => Widget.Icon({
-    visible: notifications.bind("dnd"),
-    icon: icons.notifications.silent,
-})
-
-const AudioIndicator = () => Widget.Icon()
-    .hook(audio.speaker, self => {
-        const vol = audio.speaker.is_muted ? 0 : audio.speaker.volume
-        const { muted, low, medium, high, overamplified } = icons.audio.volume
-        const cons = [[101, overamplified], [67, high], [34, medium], [1, low], [0, muted]] as const
-        self.icon = cons.find(([n]) => n <= vol * 100)?.[1] || ""
-    })
-
-export default () => PanelButton({
-    window: "ags-quicksettings",
-    on_clicked: () => App.toggleWindow("ags-quicksettings"),
-    on_scroll_up: () => audio.speaker.volume += 0.01,
-    on_scroll_down: () => audio.speaker.volume -= 0.01,
-    child: Widget.Box([
-        ProfileIndicator(),
-        ModeIndicator(),
-        DNDIndicator(),
-        AudioIndicator(),
-        MicrophoneIndicator(),
-    ]),
-})
+export default () =>
+   PanelButton({
+      window: "ags-quicksettings",
+      on_clicked: () => App.toggleWindow("ags-quicksettings"),
+      on_scroll_up: () => (audio.speaker.volume += 0.01),
+      on_scroll_down: () => (audio.speaker.volume -= 0.01),
+      child: Widget.Box([
+         ProfileIndicator(),
+         DNDIndicator(),
+         AudioIndicator(),
+         MicrophoneIndicator(),
+      ]),
+   });
