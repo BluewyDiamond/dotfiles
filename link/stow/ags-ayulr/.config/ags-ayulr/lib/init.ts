072277608e6e41a5389796cf29f0bab0e3278577
BluewyDiamond <bluewy.s.network@gmail.com>
2024-09-20 00:49:14 +0200

    feat!(ags-ayulr): remove even more stuff

--- link/stow/ags-ayulr/.config/ags-ayulr/lib/init.ts
+++ link/stow/ags-ayulr/.config/ags-ayulr/lib/init.ts
@@ -1,5 +1,4 @@
 import hyprland from "./hyprland"
-import tmux from "./tmux"
 import gtk from "./gtk"
 import lowBattery from "./battery"
 import notifications from "./notifications"
@@ -7,7 +6,6 @@ import notifications from "./notifications"
 export default function init() {
     try {
         gtk()
-        tmux()
         lowBattery()
         notifications()
         hyprland()
