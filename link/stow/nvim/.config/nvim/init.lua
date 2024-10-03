a5644cb0c3a28d1b08d5fc726f510d6e40fa0a77
BluewyDiamond <bluewy.s.network@gmail.com>
2024-10-03 02:41:44 +0200

    feat(nvim): update

--- link/stow/nvim/.config/nvim/init.lua
+++ link/stow/nvim/.config/nvim/init.lua
@@ -1,13 +1,1 @@
 require("bluewy.config")
-
--- Map <C-S-.> to cycle to the next buffer
-vim.api.nvim_set_keymap('n', '<C-S-.>', '<cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true, desc = "next buffer" })
-
--- Map <C-S-h> to cycle to the previous buffer
-vim.api.nvim_set_keymap('n', '<C-S-h>', '<cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true, desc = "previous buffer" })
-
--- Map <C-S-A-.> to move buffer to the right
-vim.api.nvim_set_keymap('n', '<C-S-A-.>', '<cmd>BufferLineMoveNext<CR>', { noremap = true, silent = true, desc = "move buffer to the right" })
-
--- Map <C-S-A-h> to move buffer to the left
-vim.api.nvim_set_keymap('n', '<C-S-A-h>', '<cmd>BufferLineMovePrev<CR>', { noremap = true, silent = true, desc = "move buffer to the left" })
