60727973f3bfe35c52a7c5939b741464527e4bd7
BluewyDiamond <bluewy.s.network@gmail.com>
2024-10-09 20:56:42 +0200

    feat(nvim): follow more closely to the example config

--- link/stow/nvim/.config/nvim/lua/bluewy/plugins/code/completions-autopairs.lua
+++ link/stow/nvim/.config/nvim/lua/bluewy/plugins/code/completions-autopairs.lua
@@ -1,106 +1,162 @@
+-- return {
+--    {
+--       "hrsh7th/nvim-cmp", --> latest commit
+--       event = { "BufReadPost", "BufNewFile" },
+--
+--       dependencies = {
+--          { "hrsh7th/cmp-nvim-lsp" }, --> latest commit
+--          { "hrsh7th/cmp-buffer" }, --> latest commit
+--          { "hrsh7th/cmp-path" }, --> latest commit
+--          { "L3MON4D3/LuaSnip", version = "2.x" },
+--          { "saadparwaiz1/cmp_luasnip" }, --> latest commit
+--          { "rafamadriz/friendly-snippets" }, --> latest commit
+--          { "onsails/lspkind.nvim" }, --> latest commit
+--          { "windwp/nvim-ts-autotag" }, --> latest commit
+--          { "windwp/nvim-autopairs" }, --> latest commit
+--       },
+--
+--       config = function()
+--          local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+--          local cmp = require("cmp")
+--          local luasnip = require("luasnip")
+--          local lspkind = require("lspkind")
+--
+--          require("nvim-autopairs").setup()
+--
+--          -- Integrate nvim-autopairs with cmp.
+--          cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+--
+--          require("luasnip.loaders.from_vscode").lazy_load()
+--
+--          cmp.setup({
+--             snippet = {
+--                expand = function(args)
+--                   luasnip.lsp_expand(args.body)
+--                end,
+--             },
+--
+--             window = {
+--                completion = cmp.config.window.bordered(),
+--                documentation = cmp.config.window.bordered(),
+--             },
+--
+--             mapping = cmp.mapping.preset.insert({
+--                ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+--                ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+--
+--                ["<Tab>"] = cmp.mapping(function(fallback)
+--                   if cmp.visible() then
+--                      cmp.select_next_item()
+--                   elseif luasnip.expand_or_jumpable() then
+--                      luasnip.expand_or_jump()
+--                   else
+--                      fallback()
+--                   end
+--                end, { "i", "s" }),
+--                ["<S-Tab>"] = cmp.mapping(function(fallback)
+--                   if cmp.visible() then
+--                      cmp.select_prev_item()
+--                   elseif luasnip.jumpable(-1) then
+--                      luasnip.jump(-1)
+--                   else
+--                      fallback()
+--                   end
+--                end, { "i", "s" }),
+--
+--                ["<C-u>"] = cmp.mapping.scroll_docs(4), -- scroll up preview
+--                ["<C-d>"] = cmp.mapping.scroll_docs(-4), -- scroll down preview
+--                ["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
+--                ["<C-c>"] = cmp.mapping.abort(), -- close completion window
+--                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- select suggestion
+--             }),
+--
+--             -- sources for autocompletion
+--             sources = cmp.config.sources({
+--                { name = "nvim_lsp" }, -- lsp
+--                { name = "buffer", max_item_count = 5 }, -- text within current buffer
+--                { name = "path", max_item_count = 3 }, -- file system paths
+--                { name = "luasnip", max_item_count = 3 }, -- snippets
+--             }),
+--
+--             -- Enable pictogram icons for lsp/autocompletion
+--             formatting = {
+--                expandable_indicator = true,
+--
+--                format = function(entry, item)
+--                   item = lspkind.cmp_format({
+--                      mode = "symbol_text",
+--                      maxwidth = 50,
+--                      ellipsis_char = "...",
+--                      symbol_map = {
+--                         Copilot = "",
+--                      },
+--                   })(entry, item)
+--
+--                   item = require("nvim-highlight-colors").format(entry, item)
+--
+--                   return item
+--                end,
+--             },
+--             experimental = {
+--                ghost_text = true,
+--             },
+--          })
+--       end,
+--    },
+-- }
+
 return {
-   {
-      "hrsh7th/nvim-cmp", --> latest commit
-      event = { "BufReadPost", "BufNewFile" },
+   "hrsh7th/nvim-cmp", --> latest commit
 
-      dependencies = {
-         { "hrsh7th/cmp-nvim-lsp" }, --> latest commit
-         { "hrsh7th/cmp-buffer" }, --> latest commit
-         { "hrsh7th/cmp-path" }, --> latest commit
-         { "L3MON4D3/LuaSnip", version = "2.x" },
-         { "saadparwaiz1/cmp_luasnip" }, --> latest commit
-         { "rafamadriz/friendly-snippets" }, --> latest commit
-         { "onsails/lspkind.nvim" }, --> latest commit
-         { "windwp/nvim-ts-autotag" }, --> latest commit
-         { "windwp/nvim-autopairs" }, --> latest commit
+   dependencies = {
+      "hrsh7th/cmp-nvim-lsp", --> latest comimt
+      "hrsh7th/cmp-path", --> latest commit
+      "windwp/nvim-autopairs", --> latest commit
+      {
+         "L3MON4D3/LuaSnip",
+         version = "2.x",
+
+         dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+         },
       },
-
-      config = function()
-         local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-         local cmp = require("cmp")
-         local luasnip = require("luasnip")
-         local lspkind = require("lspkind")
-
-         require("nvim-autopairs").setup()
-
-         -- Integrate nvim-autopairs with cmp.
-         cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-         require("luasnip.loaders.from_vscode").lazy_load()
-
-         cmp.setup({
-            snippet = {
-               expand = function(args)
-                  luasnip.lsp_expand(args.body)
-               end,
-            },
-
-            window = {
-               completion = cmp.config.window.bordered(),
-               documentation = cmp.config.window.bordered(),
-            },
-
-            mapping = cmp.mapping.preset.insert({
-               ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-               ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-
-               ["<Tab>"] = cmp.mapping(function(fallback)
-                  if cmp.visible() then
-                     cmp.select_next_item()
-                  elseif luasnip.expand_or_jumpable() then
-                     luasnip.expand_or_jump()
-                  else
-                     fallback()
-                  end
-               end, { "i", "s" }),
-               ["<S-Tab>"] = cmp.mapping(function(fallback)
-                  if cmp.visible() then
-                     cmp.select_prev_item()
-                  elseif luasnip.jumpable(-1) then
-                     luasnip.jump(-1)
-                  else
-                     fallback()
-                  end
-               end, { "i", "s" }),
-
-               ["<C-u>"] = cmp.mapping.scroll_docs(4), -- scroll up preview
-               ["<C-d>"] = cmp.mapping.scroll_docs(-4), -- scroll down preview
-               ["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
-               ["<C-c>"] = cmp.mapping.abort(), -- close completion window
-               ["<CR>"] = cmp.mapping.confirm({ select = true }), -- select suggestion
-            }),
-
-            -- sources for autocompletion
-            sources = cmp.config.sources({
-               { name = "nvim_lsp" }, -- lsp
-               { name = "buffer", max_item_count = 5 }, -- text within current buffer
-               { name = "path", max_item_count = 3 }, -- file system paths
-               { name = "luasnip", max_item_count = 3 }, -- snippets
-            }),
-
-            -- Enable pictogram icons for lsp/autocompletion
-            formatting = {
-               expandable_indicator = true,
-
-               format = function(entry, item)
-                  item = lspkind.cmp_format({
-                     mode = "symbol_text",
-                     maxwidth = 50,
-                     ellipsis_char = "...",
-                     symbol_map = {
-                        Copilot = "",
-                     },
-                  })(entry, item)
-
-                  item = require("nvim-highlight-colors").format(entry, item)
-
-                  return item
-               end,
-            },
-            experimental = {
-               ghost_text = true,
-            },
-         })
-      end,
    },
+
+   config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("nvim-autopairs").setup()
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+      cmp.setup({
+         snippet = {
+            expand = function(args)
+               require("luasnip").lsp_expand(args.body)
+            end,
+         },
+
+         window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+         },
+
+         mapping = cmp.mapping.preset.insert({
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+         }),
+
+         sources = cmp.config.sources({
+            { name = "nvim_lsp" }, -- lsp
+            { name = "buffer", max_item_count = 5 }, -- text within current buffer
+            { name = "path", max_item_count = 3 }, -- file system paths
+            { name = "luasnip", max_item_count = 3 }, -- snippets
+         }, {}),
+      })
+   end,
 }
--- link/stow/nvim/.config/nvim/lua/bluewy/plugins/code/lsp.lua
+++ link/stow/nvim/.config/nvim/lua/bluewy/plugins/code/lsp.lua
@@ -9,9 +9,7 @@ return {
    },
 
    config = function()
-      require("mason").setup({
-         PATH = "append",
-      })
+      require("mason").setup({})
 
       require("mason-lspconfig").setup({
          ensure_installed = {
@@ -19,21 +17,10 @@ return {
          },
       })
 
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-
-      if ok then
-         capabilities = cmp_nvim_lsp.default_capabilities()
-      else
-         vim.notify(
-            "Unable to load 'cmp-nvim-lsp' defaulting to none.",
-            vim.log.levels.ERROR,
-            { title = "BLUEWY'S MESSAGE" }
-         )
-      end
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       require("mason-lspconfig").setup_handlers({
-         function(server_name) -- default handler (optional)
+         function(server_name)
             require("lspconfig")[server_name].setup({
                capabilities = capabilities,
             })
