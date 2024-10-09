c273ecc742bb5ebddcf129f66df940fdd775f7a4
BluewyDiamond <bluewy.s.network@gmail.com>
2024-10-09 21:01:47 +0200

    feat(nvim): update

--- link/stow/nvim/.config/nvim/lua/bluewy/options.lua
+++ link/stow/nvim/.config/nvim/lua/bluewy/options.lua
@@ -1,28 +1,16 @@
 vim.g.mapleader = " "
-vim.o.timeoutlen = 1000
 
--- line wrapping preferences
 vim.o.wrap = false
--- if wrap is enabled:
--- vim.o.linebreak = true
--- vim.o.breakindent = true
 
--- indentation preferences
 vim.o.tabstop = 3
 vim.o.softtabstop = 3
 vim.o.shiftwidth = 3
 vim.o.expandtab = true
 
--- Use system clipboard.
 vim.o.clipboard = "unnamedplus"
 
--- other stuff
-vim.wo.relativenumber = true
+vim.o.relativenumber = true
 vim.o.cursorline = true
-
--- search stuff
-vim.o.ignorecase = true
-vim.o.smartcase = true
 
 -- whitespaces stuff
 vim.o.list = true
