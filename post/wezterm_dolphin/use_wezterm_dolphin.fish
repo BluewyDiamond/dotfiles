1623add24a8b621f1a8c87f1dd66f7ecbaadd704
BluewyDiamond <bluewy.s.network@gmail.com>
2024-10-09 15:24:50 +0200

    feat: update

--- post/wezterm_dolphin/use_wezterm_dolphin.fish
+++ post/wezterm_dolphin/use_wezterm_dolphin.fish
@@ -1,6 +1,17 @@
 #!/bin/env fish
 
+set -g SCRIPT_NAME (basename (status -f))
+
+function prompt
+    set_color magenta
+    echo -n "$SCRIPT_NAME => "
+    set_color yellow
+    echo "$argv"
+end
+
+# add custom entry to open the proper path in wezterm
 mkdir -p $HOME/.local/share/kio/servicemenus
 cp open-in-wezterm.desktop $HOME/.local/share/kio/servicemenus
 
-echo "script: add TerminalApplication=wezterm to ~/.config/kdeglobals"
+# open terminal application with preferred terminal
+prompt "Add 'TerminalApplication=wezterm' under '[General]' in ~/.config/kdeglobals."
