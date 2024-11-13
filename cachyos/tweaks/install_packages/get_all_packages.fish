f8c2dc1c7034382f915f111825752f00d65ba89e
BluewyDiamond <bluewy.s.network@gmail.com>
2024-11-13 13:48:16 +0100

    refactor

--- cachyos/tweaks/install_packages/get_all_packages.fish
+++ cachyos/tweaks/install_packages/get_all_packages.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
-set -g JSON_FILE (dirname (status -f))'/./packages2.json'
+set -g JSON_FILE (dirname (status -f))'/./packages.json'
 
 jq -r '[.. | .std? // empty | .[]] + [.. | .aur? // empty | .[]] | unique | .[]' $JSON_FILE | sort -u
