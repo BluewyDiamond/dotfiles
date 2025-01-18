0ece13cf55242cbdd5805a9eba6fb829ebcaf22b
BluewyDiamond <bluewy.s.network@gmail.com>
2025-01-18 17:26:33 +0100

    feat(nvim)

--- cachyos/files/files_for_link/config/nvim/lua/bluewy/plugins/code/lsp.lua
+++ cachyos/files/files_for_link/config/nvim/lua/bluewy/plugins/code/lsp.lua
@@ -33,31 +33,19 @@ return {
          local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
          local installed_servers = mason_lspconfig.get_installed_servers()
-         local excluded_servers = { "rust_analyzer", "ts_ls" }
 
          local override_servers = {
             rust_analyzer = function() end,
             ts_ls = function() end,
          }
 
-         local function contains(server_name)
-            for _, excluded in ipairs(excluded_servers) do
-               if excluded == server_name then
-                  return true
-               end
-            end
-            return false
-         end
-
          for _, server_name in ipairs(installed_servers) do
-            if not contains(server_name) then
-               if override_servers[server_name] then
-                  override_servers[server_name]()
-               else
-                  lspconfig[server_name].setup({
-                     capabilities = capabilities,
-                  })
-               end
+            if override_servers[server_name] then
+               override_servers[server_name]()
+            else
+               lspconfig[server_name].setup({
+                  capabilities = capabilities,
+               })
             end
          end
 
