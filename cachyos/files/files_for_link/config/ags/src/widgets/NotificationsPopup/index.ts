888f71ce959982150b67c68739283010153a422c
BluewyDiamond <bluewy.s.network@gmail.com>
2025-01-21 03:19:43 +0100

    feat(ags)

--- cachyos/files/files_for_link/config/ags/src/widgets/NotificationsPopup/index.ts
+++ cachyos/files/files_for_link/config/ags/src/widgets/NotificationsPopup/index.ts
@@ -1,12 +1,13 @@
-import { Astal, Gdk, Widget } from "astal/gtk3";
+import { Astal, Gdk, Gtk, Widget } from "astal/gtk3";
 import Notifd from "gi://AstalNotifd";
 import { NotificationMap } from "./NotificationMap";
 import { bind } from "astal";
+import { noImplicitDestroy } from "astal/_astal";
 
-function Notifications(): Widget.Box {
+export default function (gdkmonitor: Gdk.Monitor): Widget.Window {
    const notificationMap = new NotificationMap();
 
-   return new Widget.Box({
+   const mainBox = new Widget.Box({
       className: "notifications-popup-content",
       vertical: true,
       noImplicitDestroy: true,
@@ -16,35 +17,30 @@ function Notifications(): Widget.Box {
          notificationMap.destroy();
       },
    });
-}
 
-export default function (gdkmonitor: Gdk.Monitor): Widget.Window {
-   const notifd = Notifd.get_default();
-
-   return new Widget.Window({
+   const window = new Widget.Window({
       gdkmonitor: gdkmonitor,
       name: "astal-notifications-popup",
       namespace: "astal-notifications-popup",
       className: "notifications-popup",
       layer: Astal.Layer.OVERLAY,
-      //exclusivity: Astal.Exclusivity.IGNORE,
       anchor: Astal.WindowAnchor.TOP | Astal.WindowAnchor.RIGHT,
-      child: Notifications(),
-
-      setup: (self) => {
-         function onNotificationsChanged() {
-            if (notifd.notifications.length > 0) {
-               self.visible = true;
-            } else {
-               self.visible = false;
-            }
-         }
-
-         onNotificationsChanged();
-
-         self.hook(notifd, "notify::notifications", () =>
-            onNotificationsChanged()
-         );
-      },
+      child: mainBox,
    });
+
+   function onNotificationWidgetsChanged(list: Gtk.Widget[]) {
+      mainBox.children = list;
+
+      if (list.length <= 0) {
+         window.visible = false;
+      } else {
+         window.visible = true;
+      }
+   }
+
+   notificationMap.subscribe((list) => {
+      onNotificationWidgetsChanged(list);
+   });
+
+   return window;
 }
