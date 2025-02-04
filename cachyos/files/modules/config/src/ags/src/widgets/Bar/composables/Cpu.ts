60df2a56faef2d26ba7f2b45f56edb30f3864e79
BluewyDiamond <bluewy.s.network@gmail.com>
2025-02-05 00:39:27 +0100

    feat(ags)

--- cachyos/files/modules/config/src/ags/src/widgets/Bar/composables/Cpu.ts
+++ cachyos/files/modules/config/src/ags/src/widgets/Bar/composables/Cpu.ts
@@ -1,29 +1,45 @@
-import { bind, interval, Variable } from "astal";
+import { bind, interval, timeout, Variable } from "astal";
 import { Gtk, Widget } from "astal/gtk4";
 import { IconWithLabelFallback } from "../../composables/IconWithLabelFallback";
 import icons from "../../../libs/icons";
 import GTop from "gi://GTop";
 
 const INTERVAL = 2000;
-let lastCpuStats: GTop.glibtop_cpu | null = null;
-const cpuUsage: Variable<number | null> = Variable(null);
 
-interval(INTERVAL, async () => {
+function getGtopCpu(): GTop.glibtop_cpu {
    const gtopCpu = new GTop.glibtop_cpu();
    GTop.glibtop_get_cpu(gtopCpu);
+   return gtopCpu;
+}
 
-   if (lastCpuStats) {
-      cpuUsage.set(
-         1 -
-            (gtopCpu.idle - lastCpuStats.idle) /
-               (gtopCpu.total - lastCpuStats.total)
-      );
-   } else {
-      cpuUsage.set(null);
+function calculateCpuUsage(
+   now: GTop.glibtop_cpu,
+   previous: GTop.glibtop_cpu
+): number {
+   const totalDifference = now.total - previous.total;
+
+   if (totalDifference === 0) {
+      return 0;
    }
 
+   return 1 - (now.idle - previous.idle) / totalDifference;
+}
+
+let lastCpuStats: GTop.glibtop_cpu = getGtopCpu();
+
+const cpuUsage: Variable<number> = Variable(
+   calculateCpuUsage(getGtopCpu(), lastCpuStats)
+);
+
+const updateCpuUsage = () => {
+   const gtopCpu = new GTop.glibtop_cpu();
+   GTop.glibtop_get_cpu(gtopCpu);
+   cpuUsage.set(calculateCpuUsage(gtopCpu, lastCpuStats));
    lastCpuStats = gtopCpu;
-});
+};
+
+updateCpuUsage();
+timeout(INTERVAL, () => interval(INTERVAL, () => updateCpuUsage()));
 
 export default function (): Gtk.Button {
    return Widget.Button(
@@ -40,10 +56,6 @@ export default function (): Gtk.Button {
 
             Widget.Label({
                label: bind(cpuUsage).as((cpuUsage) => {
-                  if (!cpuUsage) {
-                     return "???%";
-                  }
-
                   return `${Math.ceil(cpuUsage * 100)
                      .toString()
                      .padStart(3, "_")}%`;
