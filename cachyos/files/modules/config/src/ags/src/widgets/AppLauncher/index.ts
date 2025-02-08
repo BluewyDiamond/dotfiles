d9fe620e2eeaf8fd6614a910d85d0c069cc0b806
BluewyDiamond <bluewy.s.network@gmail.com>
2025-02-08 16:09:14 +0100

    feat(ags)

--- cachyos/files/modules/config/src/ags/src/widgets/AppLauncher/index.ts
+++ cachyos/files/modules/config/src/ags/src/widgets/AppLauncher/index.ts
@@ -92,7 +92,7 @@ export default function (gdkmonitor: Gdk.Monitor): Astal.Window {
 
                Widget.Button({
                   cssClasses: ["refresh"],
-                  canFocus: false,
+                  //canFocus: false,
                   child: IconWithLabelFallback({ icon: icons.ui.refresh }),
 
                   onClicked: () => {
