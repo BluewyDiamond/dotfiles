291a8d0966c7417c71257d35475a3bee7eab35bb
BluewyDiamond <bluewy.s.network@gmail.com>
2025-02-10 20:07:19 +0100

    feat

--- cachyos/files/modules/config/src/ags/src/utils/style.ts
+++ cachyos/files/modules/config/src/ags/src/utils/style.ts
@@ -1,25 +1,27 @@
 import { exec, writeFile } from "astal";
+import options from "../options";
 
-const build = "/home/bluewy/.config/ags/build";
+const homeDir = exec([...options.sh.cmd, "echo $HOME"]);
+const buildDir = `${homeDir}/.config/ags/build`;
+
+const variables = () => [
+   //`$indicators_spacing: ${options.bar.indicators.spacing}px;`,
+   //`$workspaces_spacing: ${options.bar.workspaces.spacing}px;`,
+];
 
 export function getCss(): string {
-   const vars = `${build}/variables.scss`;
-   const scss = `${build}/main.scss`;
-   const css = `${build}/main.css`;
+   const vars = `${buildDir}/variables.scss`;
+   const scss = `${buildDir}/main.scss`;
+   const css = `${buildDir}/main.css`;
 
-   const fd = exec(`fd ".scss" /home/bluewy/.config/ags`);
+   const fd = exec(`fd ".scss" ${homeDir}/.config/ags/src/scss`);
    const files = fd.split(/\s+/);
    const imports = [vars, ...files].map((f) => `@import '${f}';`);
 
-   exec(`mkdir -p ${build}`);
+   exec(`mkdir -p ${buildDir}`);
    writeFile(vars, variables().join("\n"));
    writeFile(scss, imports.join("\n"));
    exec(`sass ${scss} ${css}`);
 
    return css;
 }
-
-const variables = () => [
-   //`$indicators_spacing: ${options.bar.indicators.spacing}px;`,
-   //`$workspaces_spacing: ${options.bar.workspaces.spacing}px;`,
-];
