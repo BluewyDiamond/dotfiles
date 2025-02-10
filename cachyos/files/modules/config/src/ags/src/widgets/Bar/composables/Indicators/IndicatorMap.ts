8209aa8c1ebeb3010a305e3bd11dfab0a279f87d
BluewyDiamond <bluewy.s.network@gmail.com>
2025-02-10 17:28:58 +0100

    feat(ags)

--- cachyos/files/modules/config/src/ags/src/widgets/Bar/composables/Indicators/IndicatorMap.ts
+++ cachyos/files/modules/config/src/ags/src/widgets/Bar/composables/Indicators/IndicatorMap.ts
@@ -1,4 +1,4 @@
-import type { Gtk } from "astal/gtk4";
+import { Widget, Gtk } from "astal/gtk4";
 import { bind, type Subscribable } from "astal/binding";
 import { Variable } from "astal";
 import PowerProfiles from "gi://AstalPowerProfiles";
@@ -7,13 +7,27 @@ import Wp from "gi://AstalWp";
 import { IconWithLabelFallback } from "../../../composables/IconWithLabelFallback";
 import Hookable from "../../../../libs/services/Hookable";
 import options from "../../../../options";
+import AstalPowerProfiles from "gi://AstalPowerProfiles?version=0.1";
 
-const powerProfiles = PowerProfiles.get_default();
+const powerProfiles =
+   PowerProfiles.get_default() as AstalPowerProfiles.PowerProfiles | null;
+
 const wp = Wp.get_default();
+const audio = wp?.get_audio();
+const microphone = audio?.get_default_microphone();
+const speaker = audio?.get_default_speaker();
+const video = wp?.get_video();
+
+enum Indicators {
+   PowerProfiles,
+   MicrophoneRecorders,
+   ScreenRecorders,
+   Speaker,
+}
 
 export class IndicatorMap extends Hookable implements Subscribable {
-   private readonly map: Map<string, Gtk.Widget | null> = new Map<
-      string,
+   private readonly map: Map<Indicators, Gtk.Widget | null> = new Map<
+      Indicators,
       Gtk.Widget | null
    >();
 
@@ -22,191 +36,10 @@ export class IndicatorMap extends Hookable implements Subscribable {
    constructor() {
       super();
 
-      {
-         const onPowerProfileChanged = (): void => {
-            const activeProfile = powerProfiles.get_active_profile();
-
-            if (activeProfile === "balanced") {
-               this.map.set("powerprofiles", null);
-               this.notify();
-            } else if (activeProfile === "powersaver") {
-               this.map.set(
-                  "powerprofiles",
-                  IconWithLabelFallback({
-                     icon: icons.powerprofile.powerSaver,
-                     symbolic: options.bar.indicators.powerprofiles.symbolic,
-                  })
-               );
-
-               this.notify();
-            } else if (activeProfile === "performance") {
-               this.map.set(
-                  "powerprofiles",
-                  IconWithLabelFallback({
-                     icon: icons.powerprofile.performance,
-                     symbolic: options.bar.indicators.powerprofiles.symbolic,
-                  })
-               );
-
-               this.notify();
-            } else {
-               this.map.set(
-                  "powerprofiles",
-                  IconWithLabelFallback({ icon: icons.broken })
-               );
-
-               this.notify();
-            }
-         };
-
-         onPowerProfileChanged();
-
-         this.hook(powerProfiles, "notify", () => {
-            onPowerProfileChanged();
-         });
-      }
-
-      {
-         const audio = wp?.get_audio();
-
-         if (audio == null) {
-            this.map.set(
-               "recoders",
-               IconWithLabelFallback({ icon: icons.broken })
-            );
-
-            this.notify();
-            return;
-         }
-
-         const mic = audio.get_default_microphone();
-
-         if (mic === null) {
-            this.map.set(
-               "recoders",
-               IconWithLabelFallback({ icon: icons.broken })
-            );
-
-            this.notify();
-            return;
-         }
-
-         this.derives.add(
-            Variable.derive(
-               [bind(audio, "recorders"), bind(mic, "volume")],
-
-               (recorders, micVolume) => {
-                  if (recorders.length > 0) {
-                     let icon;
-
-                     if (micVolume > 0.67) {
-                        icon = icons.audio.mic.high;
-                     } else if (micVolume > 0.34) {
-                        icon = icons.audio.mic.medium;
-                     } else if (micVolume > 0.1) {
-                        icon = icons.audio.mic.low;
-                     } else {
-                        icon = icons.audio.mic.muted;
-                     }
-
-                     this.map.set("recorders", IconWithLabelFallback({ icon }));
-                     this.notify();
-                  } else {
-                     this.map.set("recorders", null);
-                     this.notify();
-                  }
-               }
-            )
-         );
-      }
-
-      {
-         const audio = wp?.get_audio();
-
-         if (audio == null) {
-            this.map.set(
-               "recoders",
-               IconWithLabelFallback({ icon: icons.broken })
-            );
-
-            this.notify();
-            return;
-         }
-
-         const speaker = audio.get_default_speaker();
-
-         if (speaker === null) {
-            this.map.set(
-               "recoders",
-               IconWithLabelFallback({ icon: icons.broken })
-            );
-
-            this.notify();
-            return;
-         }
-
-         this.derives.add(
-            Variable.derive([bind(speaker, "volume")], (volume) => {
-               let icon: Icon | null = null;
-
-               const {
-                  audio: {
-                     volume: { high, medium, low, muted },
-                  },
-               } = icons;
-
-               if (volume > 0.67) {
-                  icon = high;
-               } else if (volume > 0.34) {
-                  icon = medium;
-               } else if (volume > 0.1) {
-                  icon = low;
-               } else {
-                  icon = muted;
-               }
-
-               this.map.set("speaker", IconWithLabelFallback({ icon }));
-               this.notify();
-            })
-         );
-      }
-
-      {
-         const video = wp?.get_video();
-
-         if (video == null) {
-            this.map.set(
-               "recoders",
-               IconWithLabelFallback({ icon: icons.broken })
-            );
-
-            this.notify();
-            return;
-         }
-
-         this.derives.add(
-            Variable.derive(
-               [bind(video, "recorders")],
-
-               (recorders: Wp.Endpoint[]) => {
-                  if (recorders.length > 0) {
-                     this.map.set(
-                        "screen-recorders",
-
-                        IconWithLabelFallback({
-                           icon: icons.recorder.screencast,
-                        })
-                     );
-
-                     this.notify();
-                  } else {
-                     this.map.set("screen-recorders", null);
-                     this.notify();
-                  }
-               }
-            )
-         );
-      }
+      this.setupPowerProfilesIndicator();
+      this.setupScreenRecordersIndicator();
+      this.setupMicrophoneRecordersIndicator();
+      this.setupSpeakerIndicator();
    }
 
    get(): Gtk.Widget[] {
@@ -234,5 +67,219 @@ export class IndicatorMap extends Hookable implements Subscribable {
       }
 
       this.var.set(array);
+   }
+
+   private setupPowerProfilesIndicator(): void {
+      if (powerProfiles === null) {
+         this.map.set(
+            Indicators.PowerProfiles,
+
+            Widget.Label({
+               label: `${Indicators.PowerProfiles.toString().toLowerCase()}!`,
+            })
+         );
+
+         return;
+      }
+
+      const onPowerProfileChanged = (): void => {
+         const activeProfile = powerProfiles.get_active_profile();
+
+         if (activeProfile === "balanced") {
+            this.map.set(Indicators.PowerProfiles, null);
+            this.notify();
+         } else if (activeProfile === "powersaver") {
+            this.map.set(
+               Indicators.PowerProfiles,
+
+               IconWithLabelFallback({
+                  icon: icons.powerprofile.powerSaver,
+                  symbolic: options.bar.indicators.powerprofiles.symbolic,
+               })
+            );
+
+            this.notify();
+         } else if (activeProfile === "performance") {
+            this.map.set(
+               Indicators.PowerProfiles,
+
+               IconWithLabelFallback({
+                  icon: icons.powerprofile.performance,
+                  symbolic: options.bar.indicators.powerprofiles.symbolic,
+               })
+            );
+
+            this.notify();
+         } else {
+            console.error("Unrecognized powerprofile...");
+
+            this.map.set(
+               Indicators.PowerProfiles,
+
+               Widget.Label({ label: "?" })
+            );
+
+            this.notify();
+         }
+      };
+
+      onPowerProfileChanged();
+
+      this.hook(powerProfiles, "notify", () => {
+         onPowerProfileChanged();
+      });
+   }
+
+   private setupMicrophoneRecordersIndicator(): void {
+      if (audio == null) {
+         this.map.set(
+            Indicators.MicrophoneRecorders,
+
+            Widget.Label({
+               label: `${Indicators.MicrophoneRecorders.toString().toLowerCase()}!`,
+            })
+         );
+
+         this.notify();
+         return;
+      }
+
+      if (microphone === null || microphone === undefined) {
+         this.map.set(
+            Indicators.MicrophoneRecorders,
+
+            Widget.Label({
+               label: `${Indicators.MicrophoneRecorders.toString().toLowerCase()}!`,
+            })
+         );
+
+         this.notify();
+         return;
+      }
+
+      this.derives.add(
+         Variable.derive(
+            [bind(audio, "recorders"), bind(microphone, "volume")],
+
+            (recorders, micVolume) => {
+               if (recorders.length > 0) {
+                  const {
+                     audio: {
+                        mic: { high, medium, low, muted },
+                     },
+                  } = icons;
+
+                  let icon: Icon | null = null;
+
+                  if (micVolume > 0.67) {
+                     icon = high;
+                  } else if (micVolume > 0.34) {
+                     icon = medium;
+                  } else if (micVolume > 0.1) {
+                     icon = low;
+                  } else {
+                     icon = muted;
+                  }
+
+                  this.map.set(
+                     Indicators.MicrophoneRecorders,
+                     IconWithLabelFallback({ icon })
+                  );
+                  this.notify();
+               } else {
+                  this.map.set(Indicators.MicrophoneRecorders, null);
+                  this.notify();
+               }
+            }
+         )
+      );
+   }
+
+   private setupSpeakerIndicator(): void {
+      if (audio == null) {
+         this.map.set(
+            Indicators.Speaker,
+
+            Widget.Label({
+               label: `${Indicators.Speaker.toString().toLowerCase()}!`,
+            })
+         );
+
+         this.notify();
+         return;
+      }
+
+      if (speaker === null || speaker === undefined) {
+         this.map.set(
+            Indicators.Speaker,
+
+            Widget.Label({
+               label: `${Indicators.Speaker.toString().toLowerCase()}!`,
+            })
+         );
+
+         this.notify();
+         return;
+      }
+
+      this.derives.add(
+         Variable.derive([bind(speaker, "volume")], (volume) => {
+            let icon: Icon | null = null;
+
+            const {
+               audio: {
+                  volume: { high, medium, low, muted },
+               },
+            } = icons;
+
+            if (volume > 0.67) {
+               icon = high;
+            } else if (volume > 0.34) {
+               icon = medium;
+            } else if (volume > 0.1) {
+               icon = low;
+            } else {
+               icon = muted;
+            }
+
+            this.map.set(Indicators.Speaker, IconWithLabelFallback({ icon }));
+            this.notify();
+         })
+      );
+   }
+
+   private setupScreenRecordersIndicator(): void {
+      if (video == null) {
+         this.map.set(
+            Indicators.ScreenRecorders,
+            Widget.Label({ label: `${Indicators.ScreenRecorders}!` })
+         );
+
+         this.notify();
+         return;
+      }
+
+      this.derives.add(
+         Variable.derive(
+            [bind(video, "recorders")],
+
+            (recorders: Wp.Endpoint[]) => {
+               if (recorders.length > 0) {
+                  this.map.set(
+                     Indicators.ScreenRecorders,
+
+                     IconWithLabelFallback({
+                        icon: icons.recorder.screencast,
+                     })
+                  );
+
+                  this.notify();
+               } else {
+                  this.map.set(Indicators.ScreenRecorders, null);
+                  this.notify();
+               }
+            }
+         )
+      );
    }
 }
