ecc1af6f8f914ec95471443b2bbb82e55cbebcf1
BluewyDiamond <bluewy.s.network@gmail.com>
2025-02-19 00:05:00 +0100

    feat

--- cachyos/setup.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-set current_dir (realpath (dirname (status filename)))
-
-$current_dir/files/setup.fish
-$current_dir/tweaks/change_keyboard.fish
-$current_dir/tweaks/install_packages/install_packages.fish
