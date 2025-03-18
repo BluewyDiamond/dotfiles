8232195ee2a043302453b58c25659b71caed8fd7
BluewyDiamond <bluewy.s.network@gmail.com>
2025-03-19 00:11:29 +0100

    feat

--- cachyos/files/modules/ags/src/windows/bar/composables/notificationsButton.ts
+++ cachyos/files/modules/ags/src/windows/bar/composables/notificationsButton.ts
@@ -6,31 +6,33 @@ import icons from "../../../libs/icons";
 const notifd = Notifd.get_default();
 
 export default function (): Gtk.Button {
-   return Widget.Button({
-      cssClasses: ["bar-item", "bar-item-notifications"],
+   return Widget.Button(
+      {
+         cssClasses: ["bar-item", "bar-item-notifications"],
 
-      onClicked: () => {
-         App.toggle_window("astal-notifications-overview");
-      },
+         onClicked: () => {
+            App.toggle_window("astal-notifications-overview");
+         },
 
-      setup: (self) => {
-         const onNotificationsChanged = (): void => {
-            if (notifd.notifications.length > 0) {
-               self.child = IconWithLabelFallback({
-                  icon: icons.notifications.noisy,
-               });
-            } else {
-               self.child = IconWithLabelFallback({
-                  icon: icons.notifications.normal,
-               });
-            }
-         };
+         setup: (self) => {
+            const onNotificationsChanged = (): void => {
+               if (notifd.notifications.length > 0) {
+                  self.visible = true;
+               } else {
+                  self.visible = false;
+               }
+            };
 
-         onNotificationsChanged();
-
-         hook(self, notifd, "notify::notifications", () => {
             onNotificationsChanged();
-         });
+
+            hook(self, notifd, "notify::notifications", () => {
+               onNotificationsChanged();
+            });
+         },
       },
-   });
+
+      IconWithLabelFallback({
+         icon: icons.notifications.noisy,
+      })
+   );
 }
