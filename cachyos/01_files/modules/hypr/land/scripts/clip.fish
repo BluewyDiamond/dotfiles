73c613e3dc3644ca34ad6ee139ca5d840b26b19d
BluewyDiamond <bluewy.s.network@gmail.com>
2025-05-06 19:23:59 +0200

    feat(hyprland)

--- cachyos/01_files/modules/hypr/land/scripts/clip.fish
+++ cachyos/01_files/modules/hypr/land/scripts/clip.fish
@@ -2,10 +2,10 @@
 
 set script_name (basename (status filename))
 set replays_dir $HOME/media/obs
-set timeout_in_seconds 1
+set timeout_in_seconds 2
 set now_epoch_time (date +%s)
 
-if pidof obs
+if pidof obs &>/dev/null
     set hyprctl_output (hyprctl dispatch sendshortcut ',F1, class:com.obsproject.Studio')
 
     # because hyprctl fail does not change the status
@@ -20,7 +20,7 @@ if pidof obs
     set found_latest_replay_pathname (command ls $replays_dir/Replay_* | sort -r | head -n 1)
     set found_latest_replay_basename (basename $found_latest_replay_pathname)
 
-    set found_latest_replay_datetime (string replace -r '^Replay_([0-9]{4}-[0-9]{2}-[0-9]{2})_([0-9]{2})-([0-9]{2})-([0-9]{2}).*' '$1 $2:$3:$4' $found_latest_replay_basename)
+    set found_latest_replay_datetime (string replace -r -- '^Replay_([0-9]{4}-[0-9]{2}-[0-9]{2})_([0-9]{2})-([0-9]{2})-([0-9]{2}).*' '$1 $2:$3:$4' $found_latest_replay_basename)
     if test $status -ne 0
         notify-send "$script_name" "Failed replacing string..."
     end
@@ -30,7 +30,7 @@ if pidof obs
         notify-send "$script_name" "Failed datetime conversion..."
     end
 
-    if test $found_latest_replay_epoch_time -gt $now_epoch_time
+    if test $found_latest_replay_epoch_time -ge $now_epoch_time
         notify-send "$script_name" "Clip saved."
     else
         notify-send "$script_name" "Sent clip signal but clip not found..."
