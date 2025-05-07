2702c84da1249b257afbc9aa20629e7ce1c12850
BluewyDiamond <bluewy.s.network@gmail.com>
2025-05-07 15:35:16 +0200

    feat(hyprland)

--- cachyos/01_files/modules/hypr/land/scripts/colorpicker.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-set the_script (realpath (status --current-filename))
-
-set hyprpicker_output (hyprpicker; or notify-send "$the_script" "Failed, status code non zero!")
-wl-copy $hyprpicker_output
-notify-send "$the_script" "Copied $hyprpicker_output to clipboard."
