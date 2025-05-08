83fe92cb692c6e28fac9d51d21b3740f3fa256c2
BluewyDiamond <bluewy.s.network@gmail.com>
2025-05-08 20:06:07 +0200

    feat(ags): update

--- cachyos/01_files/modules/ags/cli.fish
+++ cachyos/01_files/modules/ags/cli.fish
@@ -1,16 +1,12 @@
 #!/usr/bin/env fish
 
-set parent (realpath (dirname (status filename)))
-set build_dir $parent/build
+set parent_dir (realpath (dirname (status filename)))
+set build_dir $parent_dir/build
 set ts_for_gir @ts-for-gir/cli@4.0.0-beta.19
 set astal_gjs_dir /usr/share/astal/gjs
 set gir_dir /usr/share/gir-1.0
 
-if not test -d $parent
-    set parent $HOME/.config/ags
-end
-
-if not test -d $parent
+if not test -d $parent_dir
     echo "Paths are not valid..."
     exit 1
 end
@@ -29,7 +25,7 @@ function build
     end
 
     mkdir -p $build_dir
-    bun build --minify --target bun --external gi://\* --external file://\* --external resource://\* --external system --external console --external cairo --external gettext --outdir=$build_dir -- $parent/src/main.ts
+    bun build --minify --target bun --external gi://\* --external file://\* --external resource://\* --external system --external console --external cairo --external gettext --outdir=$build_dir -- $parent_dir/src/main.ts
 end
 
 function types
