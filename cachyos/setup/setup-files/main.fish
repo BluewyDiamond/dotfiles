a3906499fce9f43cbcc8bc46d8c4f5a0bddb1401
BluewyDiamond <bluewy.s.network@gmail.com>
2025-06-05 01:39:22 +0200

    feat(setup-files): finish main

--- cachyos/setup/setup-files/main.fish
+++ cachyos/setup/setup-files/main.fish
@@ -1,5 +1,24 @@
 #!/usr/bin/env fish
 
-source ../setup-utils/lib.fish
-
-scan N "HI=>"
+./process_ags.fish
+./process_applications.fish
+./process_bin.fish
+./process_css.fish
+./process_electron_flags.fish
+./process_fastfetch.fish
+./process_fish_bluewy.fish
+./process_fuzzel.fish
+./process_hypr.fish
+./process_kbd.fish
+./process_librewolf.fish
+./process_mangohud.fish
+./process_nvim.fish
+./process_pipewire.fish
+./process_screen_off.fish
+./process_speech_dispatcher.fish
+./process_user_dirs_dirs.fish
+./process_wayland_pipewire_idle_inhibit.fish
+./process_wezterm.fish
+./process_wireplumber.fish
+./process_xkb.fish
+./process_xkb_system_wide.fish
