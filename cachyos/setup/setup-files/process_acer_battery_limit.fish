a23cde16f230b1b2fa2bd0ba3f82a531930f43ad
BluewyDiamond <bluewy.s.network@gmail.com>
2025-06-05 20:59:04 +0200

    fix(setup-files)

--- cachyos/setup/setup-files/process_acer_battery_limit.fish
+++ cachyos/setup/setup-files/process_acer_battery_limit.fish
@@ -4,5 +4,5 @@ source ./process.fish
 
 set working_dir (./get_working_dir.fish)
 
-process true copy $working_dir/lenovo_battery_limit_service/acer_battery_limit.fish /usr/local/bin/acer_battery_limit.fish
-process true copy $working_dir/lenovo_battery_limit_service/acer_battery_limit.service /etc/systemd/system/acer_battery_limit.service; and systemctl enable acer_battery_limit
+process true copy $working_dir/acer_battery_limit_service/acer_battery_limit.fish /usr/local/bin/acer_battery_limit.fish
+process true copy $working_dir/acer_battery_limit_service/acer_battery_limit.service /etc/systemd/system/acer_battery_limit.service; and systemctl enable acer_battery_limit
