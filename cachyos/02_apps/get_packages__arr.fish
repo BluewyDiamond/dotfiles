8c340a53f309104e6803b8ef64249d8f8d53cbb4
BluewyDiamond <bluewy.s.network@gmail.com>
2025-06-05 15:55:24 +0200

    refactor

--- cachyos/02_apps/get_packages__arr.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-
-set -g JSON_FILE (dirname (status filename))'/./packages.json'
-jq -r '[.. | .std? // empty | .[]] + [.. | .aur? // empty | .[]] | unique | .[]' $JSON_FILE | sort -u
