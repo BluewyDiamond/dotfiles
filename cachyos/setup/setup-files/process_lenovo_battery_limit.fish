62fc32d961b4de4e33acffdf7cc8820dea6ebcff
BluewyDiamond <bluewy.s.network@gmail.com>
2025-06-05 20:14:12 +0200

    feat(files && setup-files)

---
+++ cachyos/setup/setup-files/process_lenovo_battery_limit.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+
+source ./process.fish
+
+set working_dir (./get_working_dir.fish)
+
+process true copy $working_dir/lenovo_battery_limit_service/lenovo_battery_limit.fish /usr/local/bin/lenovo_battery_limit.fish
+process true copy $working_dir/lenovo_battery_limit_service/lenovo_battery_limit.service /etc/systemd/system/lenovo_battery_limit.service; and systemctl enable lenovo_battery_limit
--- cachyos/setup/setup-files/process_screen_off.fish
+++ cachyos/setup/setup-files/process_screen_off.fish
@@ -4,5 +4,5 @@ source ./process.fish
 
 set working_dir (./get_working_dir.fish)
 
-process true copy $working_dir/systemd_services/screen_off.fish /usr/local/bin/screen_off.fish
-process true copy $working_dir/systemd_services/screen_off.service /etc/systemd/system/screen_off.service; and systemctl enable screen_off.service
+process true copy $working_dir/screen_off_service/screen_off.fish /usr/local/bin/screen_off.fish
+process true copy $working_dir/screen_off_service/screen_off.service /etc/systemd/system/screen_off.service; and systemctl enable screen_off.service
