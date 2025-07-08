2965cf5c85af37a2a66a28fc272eafa04dbf6f2c
BluewyDiamond <bluewy.s.network@gmail.com>
2025-07-08 20:20:51 +0200

    fix(setup-files)

--- cachyos/setup/setup-files/modules/librewolf/setup.fish
+++ cachyos/setup/setup-files/modules/librewolf/setup.fish
@@ -12,13 +12,14 @@ function install
     set source_01_filename (basename $source_01)
     set source_02_filename (basename $source_02)
 
+    prepare $target_dir/$source_01_filename
     ln -s $source_01 $target_dir/$source_01_filename
 
     for profile_dir in (find "$target_dir" -type d -name '*default-default' 2>/dev/null)
         set chrome_dir $profile_dir/chrome
 
         prepare $chrome_dir/$source_02_filename
-        ln -s $chrome_dir/$source_02_filename
+        ln -s $source_02 $chrome_dir/$source_02_filename
     end
 end
 
