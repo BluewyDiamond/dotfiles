d5a9232bf98dece5f0949d8c4dac55bd2cf491e7
BluewyDiamond <bluewy.s.network@gmail.com>
2025-07-10 22:22:19 +0200

    refactor(ags2): correct WorkspaceLabel impl

--- cachyos/setup/setup-files/modules/ags2/files/src/ui/windows/bar/composables/WorkspacesButton.tsx
+++ cachyos/setup/setup-files/modules/ags2/files/src/ui/windows/bar/composables/WorkspacesButton.tsx
@@ -1,81 +1,45 @@
 import AstalHyprland from "gi://AstalHyprland";
 import options from "../../../../options";
-import { Accessor, createBinding, createState, For, With } from "ags";
-import { Gtk } from "ags/gtk4";
+import { Accessor, createBinding, createComputed, For } from "ags";
 
 const hyprland = AstalHyprland.get_default();
 
 export default function () {
-   function onFocusedWorkspaceChanged(
-      label: Gtk.Label,
-      workspaceNumber: number
-   ): void {
-      const workspace = hyprland.get_workspace(
-         workspaceNumber
-      ) as AstalHyprland.Workspace | null;
-
-      if (workspace === null) {
-         label.cssClasses = label.cssClasses.filter(
-            (cssClass) => cssClass !== "active" && cssClass !== "occupied"
-         );
-
-         return;
-      }
-
-      const workspaceClients = workspace.get_clients();
-
-      if (workspaceClients.length > 0) {
-         label.cssClasses = [...label.cssClasses, "occupied"];
-      } else {
-         label.cssClasses = label.cssClasses.filter(
-            (cssClass) => cssClass !== "occupied"
-         );
-      }
-
-      const focusedWorkspace =
-         hyprland.get_focused_workspace() as AstalHyprland.Workspace | null;
-
-      if (focusedWorkspace === null) {
-         return;
-      }
-
-      if (focusedWorkspace.id === workspaceNumber) {
-         label.cssClasses = label.cssClasses.filter(
-            (cssClass) => cssClass !== "urgent"
-         );
-
-         label.cssClasses = [...label.cssClasses, "active"];
-      } else {
-         label.cssClasses = label.cssClasses.filter(
-            (cssClass) => cssClass !== "active"
-         );
-      }
-   }
-
-   const focusedWorkspaceAccessor = createBinding(hyprland, "focusedWorkspace");
-
-   const [theWorkspaces, _] = createState(
-      Array.from(options.hyprland.workspaces)
-   );
+   const workspaces = createComputed([], () => {
+      return options.hyprland.workspaces.map((workspaceNumber) =>
+         hyprland.get_workspace(workspaceNumber)
+      );
+   });
 
    return (
       <button cssClasses={["box", "Workspaces"]}>
-         <With value={focusedWorkspaceAccessor}>
-            {(_) => (
-               <box>
-                  <For each={theWorkspaces}>
-                     {(workspaceNumber) => (
-                        <label
-                           $={(self) =>
-                              onFocusedWorkspaceChanged(self, workspaceNumber)
-                           }
-                           label={`${workspaceNumber}`}
-                        />
-                     )}
-                  </For>
-               </box>
-            )}
-         </With>
+         <For each={workspaces}>{(i) => <WorkspaceLabel workspace={i} />}</For>
       </button>
    );
+}
+
+function WorkspaceLabel({ workspace }: { workspace: AstalHyprland.Workspace }) {
+   const focusedWorkspaceBinding = createBinding(hyprland, "focusedWorkspace");
+   const workspaceClientsBinding = createBinding(workspace, "clients");
+
+   const labelCssClasses = createComputed(
+      [workspaceClientsBinding, focusedWorkspaceBinding],
+      (clients, focusedWorkspace) => {
+         const labelCssClasses = [];
+
+         if (clients.length > 0) {
+            labelCssClasses.push("ocuppied");
+         } else {
+            labelCssClasses.push("empty");
+         }
+
+         if (focusedWorkspace.id === workspace.id) {
+            labelCssClasses.push("focused");
+         }
+
+         return labelCssClasses;
+      }
+   );
+
+   return <label cssClasses={labelCssClasses} label={`${workspace.id}`} />;
 }
