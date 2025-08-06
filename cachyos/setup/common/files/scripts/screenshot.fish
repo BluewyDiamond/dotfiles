900a81400b41661e8d09e8b6bc461d3c1ed43674
BluewyDiamond <bluewy.s.network@gmail.com>
2025-08-06 17:55:42 +0200

    feat(scripts): improve robustness

--- cachyos/setup/common/files/scripts/screenshot.fish
+++ cachyos/setup/common/files/scripts/screenshot.fish
@@ -1,46 +1,66 @@
 #!/usr/bin/env fish
 
-set SCRIPT_NAME (basename (status filename))
-set file $HOME/media/screenshots/(date +"%Y-%m-%d_%H-%M-%S").png
+set script_name (basename (status filename))
+set screenshot_path (xdg-user-dir SCREENSHOTS)
+
+if not test -d $screenshot_path
+    echo "[WARNING] screenshot path not valid, defaulting to $HOME | screenshot_path={$screenshot_path}"
+    set screenshot_path $HOME
+
+    if not test -d $screenshot_path
+        echo "[ERROR] screenshot path not valid, attempting to default also failed | screenshot_path={$screenshot_path}"
+        exit 1
+    end
+end
+
+set screenshot_pathname $screenshot_path/(date +"%Y-%m-%d_%H-%M-%S").png
 
 if not which wayshot >/dev/null
-    notify-send "$SCRIPT_NAME" "Wayshot not found..."
-    return
+    echo "[ERROR] unable to proceed without wayshot"
+    notify-send "$script_name" "Wayshot not found..."
+    exit 1
 end
 
 if string match -i -q -- $argv[1] partial
     if not which slurp >/dev/null
-        notify-send "$SCRIPT_NAME" "Slurp not found..."
+        echo "[ERROR] unable to proceed without slurp"
+        notify-send "$script_name" "Slurp not found..."
         return
     end
 
-    if wayshot -f $file -s (slurp)
-        wl-copy <$file
-        set result (notify-send Screenshot "$file" -h STRING:"image-path":"$file" --action="show_in_files=Show In Files" --action="open=Open" --action="edit=Edit")
-
-        switch $result
-            case "*show_in_files*"
-                xdg-open dirname $file
-            case "*open*"
-                xdg-open $file
-            case "*edit*"
-                satty -f $file
-        end
+    if not wayshot -f $screenshot_pathname -s (slurp)
+        echo "[ERROR] wayshot error"
+        notify-send "$script_name" "Wayshot error..."
+        exit 1
     end
 
-    wl-copy <$file
-else
-    if wayshot -f $file
-        wl-copy <$file
-        set result (notify-send Screenshot "$file" -h STRING:"image-path":"$file" --action="show_in_files=Show In Files" --action="open=Open" --action="edit=Edit")
+    wl-copy <$screenshot_pathname
+    set result (notify-send Screenshot "$screenshot_pathname" -h STRING:"image-path":"$screenshot_pathname" --action="show_in_files=Show In Files" --action="open=Open" --action="edit=Edit")
 
-        switch $result
-            case "*show_in_files*"
-                xdg-open dirname $file
-            case "*open*"
-                xdg-open $file
-            case "*edit*"
-                satty -f $file
-        end
+    switch $result
+        case "*show_in_files*"
+            xdg-open dirname $screenshot_pathname
+        case "*open*"
+            xdg-open $screenshot_pathname
+        case "*edit*"
+            satty -f $screenshot_pathname
+    end
+else
+    if wayshot -f $screenshot_pathname
+        echo "[ERROR] wayshot error"
+        notify-send "$script_name" "Wayshot error..."
+        exit 1
+    end
+
+    wl-copy <$screenshot_pathname
+    set result (notify-send Screenshot "$screenshot_pathname" -h STRING:"image-path":"$screenshot_pathname" --action="show_in_files=Show In Files" --action="open=Open" --action="edit=Edit")
+
+    switch $result
+        case "*show_in_files*"
+            xdg-open dirname $screenshot_pathname
+        case "*open*"
+            xdg-open $screenshot_pathname
+        case "*edit*"
+            satty -f $screenshot_pathname
     end
 end
