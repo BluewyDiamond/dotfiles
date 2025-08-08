d19694167e8f04b622e1bb2b29549db89551b031
BluewyDiamond <bluewy.s.network@gmail.com>
2025-08-08 20:22:17 +0200

    feat(fish_bluewy)

---
+++ cachyos/setup/common/files/fish_bluewy/conf.d/rice.fish
@@ -0,0 +1,14 @@
+function _tide_item_context
+    if set -q SSH_TTY || set -q SSH_CONNECTION
+        set -l colored_host (set_color yellow)$current_host(set_color normal)
+        _tide_print_item context (set_color yellow)"$USER""@""$hostname (ssh)"
+    else
+        set -l colored_host $current_host
+        _tide_print_item context (set_color yellow)"$USER""@""$hostname"
+    end
+end
+
+if status is-interactive
+    set -U tide_left_prompt_items time context pwd status git newline character
+    set -U tide_right_prompt_items
+end
