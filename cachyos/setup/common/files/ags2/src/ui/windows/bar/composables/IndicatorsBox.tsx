fd5bd0a94bdd3fa4e39162126260d866f2ac28c1
BluewyDiamond <bluewy.s.network@gmail.com>
2025-08-09 21:19:19 +0200

    feat(ags2)

--- cachyos/setup/common/files/ags2/src/ui/windows/bar/composables/IndicatorsBox.tsx
+++ cachyos/setup/common/files/ags2/src/ui/windows/bar/composables/IndicatorsBox.tsx
@@ -1,28 +1,23 @@
-import { createBinding, createComputed, createExternal, With } from "ags";
+import {
+   Accessor,
+   createBinding,
+   createComputed,
+   createExternal,
+   With,
+} from "ags";
 import AstalPowerProfiles from "gi://AstalPowerProfiles";
 import AstalWp from "gi://AstalWp";
 import options from "../../../../options";
 import AstalNotifd from "gi://AstalNotifd";
+import Adw from "gi://Adw";
 
 const powerprofiles = AstalPowerProfiles.get_default();
 const wp = AstalWp.get_default();
+const audio = wp.get_audio();
+const video = wp.get_video();
 const notifd = AstalNotifd.get_default();
 
 export default function () {
-   // powerprofile indicator state
-   const activeProfileBinding = createBinding(powerprofiles, "activeProfile");
-
-   // microphone recorders indicator state
-   if (wp === null) {
-      return;
-   }
-
-   const audio = wp.get_audio();
-
-   if (audio === null) {
-      return;
-   }
-
    const defaultMicrophoneExternal = createExternal<null | AstalWp.Endpoint>(
       null,
       (set) => {
@@ -54,133 +49,257 @@ export default function () {
       }
    );
 
-   const microphoneRecordersBinding = createBinding(audio, "recorders");
+   const defaultSpeakerExternal = createExternal<null | AstalWp.Endpoint>(
+      null,
+      (set) => {
+         const onMicrophonesChanged = () => {
+            const speakers = audio.get_speakers();
+            if (speakers === null) return;
 
-   // screen recorder indicator state
-   const video = wp.get_video();
+            const foundDefaultSpeaker = speakers.find(
+               (speaker) => speaker.isDefault
+            );
 
-   if (video === null) {
-      return;
-   }
+            if (foundDefaultSpeaker === undefined) {
+               return;
+            }
 
-   const videoRecordersBinding = createBinding(video, "recorders");
+            set(foundDefaultSpeaker);
+         };
 
-   // speaker indicator state
-   const speaker = wp.get_default_speaker();
+         const speakersConnectionId = audio.connect("notify::speakers", (_) => {
+            onMicrophonesChanged();
+         });
 
-   if (speaker === null) {
-      return;
-   }
+         onMicrophonesChanged();
 
-   const speakerVolumeBinding = createBinding(speaker, "volume");
-   const notificationsBinding = createBinding(notifd, "notifications");
+         return () => audio.disconnect(speakersConnectionId);
+      }
+   );
 
-   const forMicrophoneImage = createComputed([
-      microphoneRecordersBinding,
-      defaultMicrophoneExternal,
-   ]);
+   const forNotificationsIndicatorComputed = createComputed<
+      [Accessor<AstalNotifd.Notification[]>],
+      [AstalNotifd.Notification[]],
+      [boolean, string | null]
+   >(
+      [createBinding(notifd, "notifications")],
 
-   return (
-      <box cssClasses={["indicators-box"]}>
-         <With value={activeProfileBinding}>
-            {(activeProfile) => {
-               let iconName = "image-missing";
+      (notifications) => {
+         if (notifications.length <= 0) {
+            return [false, null];
+         }
 
-               if (activeProfile === "powersaving") {
-                  iconName =
-                     options.bar.indicators.powerprofile.icons.powerSaver;
-               } else if (activeProfile === "balanced") {
-                  return;
-                  // iconName = options.bar.indicators.powerprofile.icons.balanced;
-               } else if (activeProfile === "performance") {
-                  iconName =
-                     options.bar.indicators.powerprofile.icons.performance;
-               }
+         return [
+            true,
+            options.bar.indicators.notifications.icons.notificationNoisy,
+         ];
+      }
+   );
 
-               return <image iconName={iconName} />;
-            }}
-         </With>
+   const forVideoRecordersIndicatorComputed = createComputed<
+      [Accessor<AstalWp.Stream[]>],
+      [AstalWp.Stream[]],
+      [boolean, string | null]
+   >([createBinding(video, "recorders")], (videoRecorders) => {
+      if (videoRecorders.length <= 0) {
+         return [false, null];
+      }
 
-         <With value={forMicrophoneImage}>
-            {(forMicrophoneImage) => {
-               const [microphoneRecorders, defaultMicrophone] =
-                  forMicrophoneImage;
+      return [true, options.bar.indicators.icons.screenshare];
+   });
 
-               if (microphoneRecorders.length <= 0) return;
+   const forMicrophoneRecordersIndicatorComputed = createComputed<
+      [Accessor<AstalWp.Stream[]>, Accessor<AstalWp.Endpoint | null>],
+      [AstalWp.Stream[], AstalWp.Endpoint | null],
+      [boolean, (() => Accessor<string>) | null]
+   >(
+      [createBinding(audio, "recorders"), defaultMicrophoneExternal],
 
-               if (defaultMicrophone === null) {
-                  return <label label={"idk"} />;
-               }
+      (microphoneRecorders, defaultMicrophone) => {
+         if (microphoneRecorders.length <= 0) {
+            return [false, null];
+         }
 
-               const volumeBinding = createBinding(defaultMicrophone, "volume");
+         if (defaultMicrophone === null) {
+            return [true, null];
+         }
 
-               const iconName = createComputed([volumeBinding], (volume) => {
-                  if (volume > 0.67) {
+         const getDefaultMicrophoneVolumeIconComputed = () => {
+            return createComputed(
+               [createBinding(defaultMicrophone, "volume")],
+               (defaultMicrophoneVolume) => {
+                  if (defaultMicrophoneVolume > 0.67) {
                      return options.bar.indicators.microphoneRecorders.icons
                         .microphoneHigh;
-                  } else if (volume > 0.34) {
+                  } else if (defaultMicrophoneVolume > 0.34) {
                      return options.bar.indicators.microphoneRecorders.icons
                         .microphoneMedium;
-                  } else if (volume > 0.1) {
+                  } else if (defaultMicrophoneVolume > 0.1) {
                      return options.bar.indicators.microphoneRecorders.icons
                         .microphoneLow;
                   } else {
                      return options.bar.indicators.microphoneRecorders.icons
                         .microphoneMuted;
                   }
-               });
-
-               return <image iconName={iconName} />;
-            }}
-         </With>
-
-         <With value={videoRecordersBinding}>
-            {(videoRecorders) => {
-               if (videoRecorders.length <= 0) {
-                  return;
                }
+            );
+         };
 
-               return (
-                  <image iconName={options.bar.indicators.icons.screenshare} />
-               );
-            }}
-         </With>
+         return [true, getDefaultMicrophoneVolumeIconComputed];
+      }
+   );
 
-         <With value={speakerVolumeBinding}>
-            {(speakerVolume) => {
-               let iconName = "";
+   const forPowerProfileIndicatorComputed = createComputed<
+      [Accessor<string>],
+      [string],
+      [boolean, string | null]
+   >([createBinding(powerprofiles, "activeProfile")], (activePowerProfile) => {
+      if (activePowerProfile === "performance") {
+         return [true, options.bar.indicators.powerprofile.icons.performance];
+      } else if (activePowerProfile === "balanced") {
+         return [false, options.bar.indicators.powerprofile.icons.performance];
+      } else if (activePowerProfile === "powersaving") {
+         return [true, options.bar.indicators.powerprofile.icons.powerSaver];
+      } else {
+         return [false, null];
+      }
+   });
 
-               if (speakerVolume <= 0) {
-                  iconName = options.bar.indicators.speaker.icons.speakerMuted;
-               } else if (speakerVolume <= 0.34) {
-                  iconName = options.bar.indicators.speaker.icons.speakerLow;
-               } else if (speakerVolume <= 0.67) {
-                  iconName = options.bar.indicators.speaker.icons.speakerMedium;
-               } else if (speakerVolume <= 1) {
-                  iconName = options.bar.indicators.speaker.icons.speakerHigh;
+   // TODO: the recorders part lol (the name will be misleading for now)
+   // also forget about default since apps can connect to non default
+   // we have to check for any connections to any speaker
+   // something akin to microphone speaker but for speaker
+   // tldr: missing speakerRecorders parameter
+   const forSpeakerRecordersIndicatorComputed = createComputed<
+      [Accessor<AstalWp.Endpoint | null>],
+      [AstalWp.Endpoint | null],
+      [boolean, (() => Accessor<string>) | null]
+   >([defaultSpeakerExternal], (defaultSpeaker) => {
+      if (defaultSpeaker === null) {
+         return [true, null];
+      }
+
+      const getDefaultVolumeIconComputed = () => {
+         return createComputed(
+            [createBinding(defaultSpeaker, "volume")],
+
+            (defaultSpeakerVolume) => {
+               if (defaultSpeakerVolume <= 0) {
+                  return options.bar.indicators.speaker.icons.speakerMuted;
+               } else if (defaultSpeakerVolume <= 0.34) {
+                  return options.bar.indicators.speaker.icons.speakerLow;
+               } else if (defaultSpeakerVolume <= 0.67) {
+                  return options.bar.indicators.speaker.icons.speakerMedium;
+               } else if (defaultSpeakerVolume <= 1) {
+                  return options.bar.indicators.speaker.icons.speakerHigh;
                } else {
-                  iconName =
-                     options.bar.indicators.speaker.icons.speakerOveramplified;
+                  return options.bar.indicators.speaker.icons
+                     .speakerOveramplified;
                }
+            }
+         );
+      };
 
-               return <image iconName={iconName} />;
-            }}
-         </With>
+      return [true, getDefaultVolumeIconComputed];
+   });
 
-         <With value={notificationsBinding}>
-            {(notifications) => {
-               if (notifications.length > 0) {
+   return (
+      <box cssClasses={["indicators-box"]}>
+         <Adw.Bin>
+            <With value={forNotificationsIndicatorComputed}>
+               {(forNotificationsIndicator) => {
+                  const [visible, iconName] = forNotificationsIndicator;
+
+                  if (!visible) {
+                     return;
+                  }
+
+                  if (iconName === null) {
+                     return;
+                  }
+
+                  return <image iconName={iconName} />;
+               }}
+            </With>
+         </Adw.Bin>
+
+         <Adw.Bin>
+            <With value={forVideoRecordersIndicatorComputed}>
+               {(forVideoRecordersIndicator) => {
+                  const [visible, iconName] = forVideoRecordersIndicator;
+
+                  if (!visible) {
+                     return;
+                  }
+
+                  if (iconName === null) {
+                     return;
+                  }
+
+                  return <image iconName={iconName} />;
+               }}
+            </With>
+         </Adw.Bin>
+
+         <Adw.Bin>
+            <With value={forMicrophoneRecordersIndicatorComputed}>
+               {(forMicrophoneRecordersIndicator) => {
+                  const [visible, getDefaultMicrophoneVolumeComputed] =
+                     forMicrophoneRecordersIndicator;
+
+                  if (!visible) {
+                     return;
+                  }
+
+                  if (getDefaultMicrophoneVolumeComputed === null) {
+                     return <label label="no default mic :3" />;
+                  }
+
                   return (
-                     <image
-                        iconName={
-                           options.bar.indicators.notifications.icons
-                              .notificationNoisy
-                        }
-                     />
+                     <image iconName={getDefaultMicrophoneVolumeComputed()} />
                   );
-               }
-            }}
-         </With>
+               }}
+            </With>
+         </Adw.Bin>
+
+         <Adw.Bin>
+            <With value={forPowerProfileIndicatorComputed}>
+               {(forPowerProfileIndicator) => {
+                  const [visible, iconName] = forPowerProfileIndicator;
+
+                  if (!visible) {
+                     return;
+                  }
+
+                  if (iconName === null) {
+                     return;
+                  }
+
+                  return <image iconName={iconName} />;
+               }}
+            </With>
+         </Adw.Bin>
+
+         <Adw.Bin>
+            <With value={forSpeakerRecordersIndicatorComputed}>
+               {(forSpeakerRecordersIndicator) => {
+                  const [visible, getDefaultSpeakerVolumeIconComputed] =
+                     forSpeakerRecordersIndicator;
+
+                  if (!visible) {
+                     return;
+                  }
+
+                  if (getDefaultSpeakerVolumeIconComputed === null) {
+                     return <label label="no default speaker :3" />;
+                  }
+
+                  return (
+                     <image iconName={getDefaultSpeakerVolumeIconComputed()} />
+                  );
+               }}
+            </With>
+         </Adw.Bin>
       </box>
    );
 }
