de6b5c4dedde1b48701242e2c90bacce294e5cb4
BluewyDiamond <bluewy.s.network@gmail.com>
2025-08-11 19:09:59 +0200

    feat(ags2)

--- cachyos/setup/common/files/ags2/cli.fish
+++ cachyos/setup/common/files/ags2/cli.fish
@@ -1,79 +1,87 @@
 #!/usr/bin/env fish
 
-set parent_dir (realpath (dirname (status filename)))
-set build_dir $parent_dir/build
-set ts_for_gir @ts-for-gir/cli@4.0.0-beta.19
-set astal_gjs_dir /usr/share/astal/gjs
-set gir_dir /usr/share/gir-1.0
+set script_path (realpath (dirname (status filename)))
+set build_pathname $script_path/build
+set build_bin_pathname $script_path/build/main.js
 
-if not test -d $parent_dir
-    echo "Paths are not valid..."
+# specifically this script requirements
+#
+set required_packages bun
+set required_packages_to_install
+
+for required_package in $required_packages
+    if not which $required_package 2&>/dev/null
+        set -a required_packages_to_install $required_package
+    end
+end
+
+if set -q required_packages_to_install[1]
+    echo "[ERROR] dependencies={$required_packages_to_install} missing"
     exit 1
 end
 
-function types
-    if test -d @girs
-        rm -r @girs
-    end
-
-    if not test -d node_modules
-        mkdir -p node_modules
-    end
-
-    if test -d node_modules/astal
-        rm node_modules/astal
-    end
-
-    ln -s $astal_gjs_dir node_modules/astal
-
-    bunx $ts_for_gir generate \* --ignore Gtk3 --ignore Astal3 --ignoreVersionConflicts --outdir "./@girs" \
-        -g $gir_dir
-end
-
+# main
+#
 switch $argv[1]
     case run
-        LD_PRELOAD=/usr/lib/libgtk4-layer-shell.so gjs -m $build_dir/main.js
+        LD_PRELOAD=/usr/lib/libgtk4-layer-shell.so gjs -m $build_pathname/main.js
     case drun
-        LD_PRELOAD=/usr/lib/libgtk4-layer-shell.so gjs -m $build_dir/main.js & disown && exit
+        LD_PRELOAD=/usr/lib/libgtk4-layer-shell.so gjs -m $build_pathname/main.js & disown && exit
     case build
-        if test -e $build_dir/main.js
-            rm $build_dir/main.js
+        if not test -d $build_pathname
+            mkdir -p $build_pathname
         end
 
-        mkdir -p $build_dir
+        if test -f $build_bin_pathname
+            rm $build_bin_pathname
+        end
 
         switch $argv[2]
             case release
-                bun run $parent_dir/build_release.ts
+                bun run $script_path/build_release.ts
             case '*'
-                bun run $parent_dir/build.ts
+                bun run $script_path/build.ts
+        end
+    case types
+        # ags reexports astal
+        set ags_js_package_config_path /usr/share/ags/js
+        set gnim_package_config_path /usr/share/ags/js/node_modules/gnim
+
+        if not $ags_js_package_config_path
+            echo "[ERROR] path={$ags_js_package_config_path} not found"
+            exit 1
         end
 
-        # switch $argv[2]
-        #     case --with-bun # currently lacking as it isn't setup to use with scss
-        #         bun build \
-        #             --production \
-        #             --minify \
-        #             --target bun \
-        #             --external gi://\* \
-        #             --external file://\* \
-        #             --external resource://\* \
-        #             --external system \
-        #             --external console \
-        #             --external cairo \
-        #             --external gettext \
-        #             --outdir=$build_dir \
-        #             -- $parent_dir/src/main.ts
-        #     case '*'
-        #         bun run $parent_dir/build.ts
-        # end
-    case types
-        bunx -y $ts_for_gir generate Gtk\* \
+        if not $gnim_package_config_path
+            echo "[ERROR] path={$gnim_package_config_path} not found"
+            exit 1
+        end
+
+        set node_modules_pathname $script_path/node_modules
+        set ags_node_module_pathname $node_modules_pathname/ags
+        set gnim_node_module_pathname $$node_modules_pathname/gnim
+
+        if not test -d $node_modules_pathname
+            mkdir -p $node_modules_pathname
+        end
+
+        if test -d $ags_js_package_config_path
+            rm $ags_js_package_config_path
+            ln -s $ags_js_package_config_path $ags_node_module_pathname
+        end
+
+        if test -d $gnim_node_module_pathname
+            rm $gnim_node_module_pathname
+            ln -s $$gnim_package_config_path $gnim_node_module_pathname
+        end
+
+        bunx -y @ts-for-gir/cli generate '*' \
+            --ignore Gtk3 --ignore Astal3 \
             --ignoreVersionConflicts \
-            --outdir $parent_dir/@girs \
+            --outdir $script_path/@girs \
             -g /usr/local/share/gir-1.0 \
             -g /usr/share/gir-1.0 \
-            -g /usr/share/\*/gir-1.0 \
-            case '*'
-        echo "Unknown option..."
+            -g '/usr/share/*/gir-1.0'
+    case '*'
+        echo "[ERROR] unknown option"
 end
